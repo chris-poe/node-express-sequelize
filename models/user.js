7a7fbdfc26d6b5d92f52e119917543893a03183a
chris-poe <chris.poe92@gmail.com>
2018-08-06 10:22:49 -0400

    Hash password in user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 'use strict';
+
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
   });
+
+  User.hook('beforeCreate', (User, options) => {
+  	bcrypt.hash(User.password, 10, (err, hash) => {
+		  return User.update({
+		    password: hash
+		  });
+		});
+	});
 
   return User;
 };
