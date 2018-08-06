b4d3fcc1a9fe5afd149f69d631ad918d9f91f3d9
chris-poe <chris.poe92@gmail.com>
2018-08-06 10:21:56 -0400

    Clear migrations

--- models/user.js
+++ models/user.js
@@ -1,20 +1,8 @@
 'use strict';
-
-const bcrypt = require('bcrypt');
-
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING
   });
-
-  User.hook('beforeCreate', (User, options) => {
-  	bcrypt.hash(User.password, 10, (err, hash) => {
-		  return User.update({
-		    password: hash
-		  });
-		});
-	});
 
   return User;
 };
